ca57ac3d18491a56a03a98f5e398bcbe57903af3
crockent <echatzakis1@isc.tuc.gr>
2023-11-10 15:47:39 +0200

    sys_ThreadExit, sys_Exit, initialize_thread, start_ptcb_main_thread
    1) Δημιουργηθηκε μια νέα συνάρτηση που κάνει αρχικοποίηση ενα νεο thread σε ενα ptcb (χρησιμοποιηθηκε στην sys_Exit)
    2) Εγιναν αλλαγές στην sys_ExIt αλλα δεν εχει ολοκληρωθει
    3) Γραφτηκε μεγάλο μέρος της sys_ThreadExit  αλλα λείπει ένα μερος
    4) Δημιουργηκε η start_ptcb_main_thread που έχει την ιδια λειτουργια με την start_main_thread απλα ειναι για multithreading

--- kernel_proc.c
+++ kernel_proc.c
@@ -127,6 +127,22 @@ void start_main_thread()
   exitval = call(argl,args);
   Exit(exitval);
 }
+/*
+  New start_main_thread for multithreading system
+*/
+
+void start_ptcb_main_thread(){
+  int exitval;
+  Task call = cur_thread->main_task;
+  int argl - cur_thread->argl;
+  void* args = cur_thread->args;
+
+  exitval = call(argl, args);
+
+  ThreadExit(exitval);
+
+}
+
 
 
 /*
@@ -182,9 +198,8 @@ Pid_t sys_Exec(Task call, int argl, void* args)
     the initialization of the PCB.
    */
   if(call != NULL) {
-    newproc->main_thread = spawn_thread(newproc, start_main_thread);
-    acquire_PTCB(newproc->main_thread,call,argl,args);
-    wakeup(newproc->main_thread);
+   
+    wakeup(newproc->main_thread=initialize_thread(newproc,start_main_thread,call,argl,args));
   }
 
 
@@ -306,60 +321,12 @@ void sys_Exit(int exitval)
 
     while(sys_WaitChild(NOPROC,NULL)!=NOPROC);
 
-  } else {
+  } 
 
-    /* Reparent any children of the exiting process to the 
-       initial task */
-    PCB* initpcb = get_pcb(1);
-    while(!is_rlist_empty(& curproc->children_list)) {
-      rlnode* child = rlist_pop_front(& curproc->children_list);
-      child->pcb->parent = initpcb;
-      rlist_push_front(& initpcb->children_list, child);
-    }
-
-    /* Add exited children to the initial task's exited list 
-       and signal the initial task */
-    if(!is_rlist_empty(& curproc->exited_list)) {
-      rlist_append(& initpcb->exited_list, &curproc->exited_list);
-      kernel_broadcast(& initpcb->child_exit);
-    }
-
-    /* Put me into my parent's exited list */
-    rlist_push_front(& curproc->parent->exited_list, &curproc->exited_node);
-    kernel_broadcast(& curproc->parent->child_exit);
-
-  }
-
-  assert(is_rlist_empty(& curproc->children_list));
-  assert(is_rlist_empty(& curproc->exited_list));
+  sys_ThreadExit(exitval)
 
 
-  /* 
-    Do all the other cleanup we want here, close files etc. 
-   */
 
-  /* Release the args data */
-  if(curproc->args) {
-    free(curproc->args);
-    curproc->args = NULL;
-  }
-
-  /* Clean up FIDT */
-  for(int i=0;i<MAX_FILEID;i++) {
-    if(curproc->FIDT[i] != NULL) {
-      FCB_decref(curproc->FIDT[i]);
-      curproc->FIDT[i] = NULL;
-    }
-  }
-
-  /* Disconnect my main_thread */
-  curproc->main_thread = NULL;
-
-  /* Now, mark the process as exited. */
-  curproc->pstate = ZOMBIE;
-
-  /* Bye-bye cruel world */
-  kernel_sleep(EXITED, SCHED_USER);
 }
 
 
