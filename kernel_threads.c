609798d3aa5c35df573a3eabc94c5fb6fa191ad8
antonismou <antonis_kostantinos@hotmail.gr>
2023-11-17 13:25:22 +0200

    add .gdbinit and update kernel_thread
    add at kernel_thread at exit
    a while loop to clean all the ptcb
    and change all CURPROC to proc
    also change return 1 to 0 at detach

--- kernel_threads.c
+++ kernel_threads.c
@@ -16,7 +16,7 @@ Tid_t sys_CreateThread(Task task, int argl, void* args)
 
   wakeup(tcb);
 
-  return (Tid_t)tcb->ptcb;
+  return (Tid_t) (tcb->ptcb);
 }
 
 /**
@@ -24,7 +24,7 @@ Tid_t sys_CreateThread(Task task, int argl, void* args)
  */
 Tid_t sys_ThreadSelf()
 {
-	return (Tid_t) cur_thread();
+	return (Tid_t) cur_thread()->ptcb;
 }
 
 /**
@@ -41,22 +41,24 @@ int sys_ThreadJoin(Tid_t tid, int* exitval)
   */
 int sys_ThreadDetach(Tid_t tid)
 {
-  PTCB* ptcb= (PTCB*) tid;//nomizo pos etsi briskoume to tid
+  PTCB* ptcb= (PTCB*) tid;
+  PCB* curproc = CURPROC;
   /*We check if this ptcb is on the list of this pcb 
   if isn't we return -1
   also we check if this thread is exited and also we return -1
   */
-  if(!(rlist_find(&(CURPROC->ptcb_list),ptcb,NULL)) ||
-      !(ptcb->exited)){
+  if((rlist_find(&(curproc->ptcb_list),ptcb,NULL)) == NULL ||
+      (ptcb->exited) == 1 ){
     return -1;
   }
+
   /*else we change the flag detached to true and
   broadcast all the threads that waitting
   */
   ptcb->detached = 1;
   CondVar* toBroadcast = &(ptcb->exit_cv);
   kernel_broadcast(toBroadcast);
-	return 1;
+	return 0;
 }
 
 /**
@@ -83,54 +85,63 @@ void sys_ThreadExit(int exitval)
 
     /* Reparent any children of the exiting process to the 
        initial task */
-  if(CURPROC->thread_count==0){
-    PCB* initpcb = get_pcb(1);
-    while(!is_rlist_empty(& CURPROC->children_list)) {
-      rlnode* child = rlist_pop_front(& CURPROC->children_list);
-      child->pcb->parent = initpcb;
-      rlist_push_front(& initpcb->children_list, child);
+  if(proc->thread_count==0){
+    if(get_pid(proc) != 1){
+      PCB* initpcb = get_pcb(1);
+      while(!is_rlist_empty(& proc->children_list)) {
+        rlnode* child = rlist_pop_front(& proc->children_list);
+        child->pcb->parent = initpcb;
+        rlist_push_front(& initpcb->children_list, child);
+      }
+
+      /* Add exited children to the initial task's exited list 
+        and signal the initial task */
+      if(!is_rlist_empty(& proc->exited_list)) {
+        rlist_append(& initpcb->exited_list, &proc->exited_list);
+        kernel_broadcast(& initpcb->child_exit);
+      }
+
+      /* Put me into my parent's exited list */
+      rlist_push_front(& proc->parent->exited_list, &proc->exited_node);
+      kernel_broadcast(& proc->parent->child_exit);
     }
-
-    /* Add exited children to the initial task's exited list 
-       and signal the initial task */
-    if(!is_rlist_empty(& CURPROC->exited_list)) {
-      rlist_append(& initpcb->exited_list, &CURPROC->exited_list);
-      kernel_broadcast(& initpcb->child_exit);
-    }
-
-    /* Put me into my parent's exited list */
-    rlist_push_front(& CURPROC->parent->exited_list, &CURPROC->exited_node);
-    kernel_broadcast(& CURPROC->parent->child_exit);
-
   
 
-  assert(is_rlist_empty(& CURPROC->children_list));
-  assert(is_rlist_empty(& CURPROC->exited_list));
+  assert(is_rlist_empty(& proc->children_list));
+  assert(is_rlist_empty(& proc->exited_list));
 
 
   /* 
     Do all the other cleanup we want here, close files etc. 
    */
-
-  /* Release the args data */
-  if(CURPROC->args) {
-    free(CURPROC->args);
-    CURPROC->args = NULL;
-  }
-
-  /* Clean up FIDT */
-  for(int i=0;i<MAX_FILEID;i++) {
-    if(CURPROC->FIDT[i] != NULL) {
-      FCB_decref(CURPROC->FIDT[i]);
-      CURPROC->FIDT[i] = NULL;
+    /*Clean up all PTCB in the ptcb_list
+    we check for the ptcb_list for nodes if is empty we cnotninue
+    else we pop clean up and we check again
+    */
+    while(is_rlist_empty(&proc->ptcb_list) != 0){
+      rlnode* ptcb_node;
+      ptcb_node = rlist_pop_front(&proc->ptcb_list);
+      free(ptcb_node->ptcb);
     }
-  }
+    /* Release the args data */
+    if(proc->args) {
+      free(proc->args);
+      proc->args = NULL;
+    }
 
-  /* Disconnect my main_thread */
-  CURPROC->main_thread = NULL;
+    /* Clean up FIDT */
+    for(int i=0;i<MAX_FILEID;i++) {
+      if(proc->FIDT[i] != NULL) {
+        FCB_decref(proc->FIDT[i]);
+        proc->FIDT[i] = NULL;
+      }
+    }
 
-  /* Now, mark the process as exited. */
-  CURPROC->pstate = ZOMBIE;
+    /* Disconnect my main_thread */
+    proc->main_thread = NULL;
+
+    /* Now, mark the process as exited. */
+    proc->pstate = ZOMBIE;
   }
 
   /* Bye-bye cruel world */
