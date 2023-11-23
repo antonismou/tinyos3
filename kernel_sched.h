0d3ab289e2356db5f50cf90acbd32a9099836840
antonismou <antonis_kostantinos@hotmail.gr>
2023-11-23 15:18:56 +0200

    Revert "Revert "Merge pull request #18 from antonismou/ManosTest""
    This reverts commit d55733b9d550c9e5efe10f4a0480e3b03d8697a1.

--- kernel_sched.h
+++ kernel_sched.h
@@ -116,7 +116,8 @@ typedef struct process_thread_control_block
 
   rlnode ptcb_node;
 } PTCB;
-TCB* initialize_thread(TCB* new_tcb,PCB* proc, void (*func)(),Task call, int argl , void* args );
+
+TCB* initialize_thread(TCB* new_tcb,PCB* proc, void (*func)(),Task task, int argl , void* args );
 void acquire_PTCB(TCB* tcb, Task task, int argl,void* args);
 
 typedef struct thread_control_block {
