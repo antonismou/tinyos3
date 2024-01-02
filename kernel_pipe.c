04b7b8384b97f4494a95368660c567f633c14a74
Alexoula <alexoylaa@gmail.com>
2024-01-02 21:28:19 +0200

    Update kernel_pipe.c
    new

--- kernel_pipe.c
+++ kernel_pipe.c
@@ -87,12 +87,62 @@ int pipe_writer_close(void* pipe_cb){
 	return 0;
 }
 
-int pipe_read(){
-	return -1;
-}
+int pipe_read(void* pipecb_t, char *buf, unsigned int n){
 
-int pipe_write(void* write_index,const char* buf, unsigned int size){
-	PIPE_CB* pipe = (PIPE_CB*) write_index;
+	PIPE_CB* pipe = (PIPE_CB*) pipecb_t;
+
+	/* Check if the pipe, the reader, or the destination buffer is closed or invalid.
+       If any of them is closed or invalid, return an error (-1) */
+	if(pipe==NULL || pipe->reader==NULL || buf==NULL){
+		return -1;
+	}
+
+	/* Calculate the number of bytes available to read from the pipe buffer */ 
+	unsigned int availableBytesToRead = PIPE_BUFFER_SIZE - pipe->empty_space;
+
+	/* If the write end is closed and the pipe buffer is empty, return 0 */
+	if(pipe->writer==NULL && availableBytesToRead == 0){
+		return 0;
+	}
+
+	/* In this while-loop as long as the pipe buffer is empty
+	   we signal the writers to write some data before we can read again */
+	while(availableBytesToRead == 0){ // xreiazetai na tsekaroume k writer!= NULL?
+
+		/* block reader(s) until data for reading are available in pipe buffer */
+		kernel_wait(&pipe->is_empty, SCHED_PIPE); 
+
+		/* After the writer writes some data the w_position and r_positiot change
+		   so we get a new value for 'availableBytesToRead' */
+		availableBytesToRead = PIPE_BUFFER_SIZE - pipe->empty_space;
+	}
+
+	/* Determine the number of bytes to read, considering 
+	   the 'availableBytesToRead' from pipe buffer and requested size 'n' */
+	unsigned int numBytesToRead;
+
+	if(n<availableBytesToRead){
+		numBytesToRead = n; // Read only 'n' number of characters
+	} else{
+		numBytesToRead = availableBytesToRead;
+	}
+
+	/* Read operation: 
+	   Copy 'numBytesToRead' bytes one by one from the pipe buffer to 'buf' */
+	for (int i=0; i < numBytesToRead; i++) {
+		buf[i] = pipe->buffer[pipe->r_position];
+		pipe->r_position = (pipe->r_position+1) % PIPE_BUFFER_SIZE; // Update read position in cyclic buffer
+		pipe->empty_space++; // Increase empty space in the buffer
+	}
+
+	/* Wake up writers to signal available space in the buffer */
+	kernel_broadcast(&pipe->is_full);
+
+	return numBytesToRead; // Return the number of bytes read
+} 
+
+int pipe_write(void* pipecb_t,const char* buf, unsigned int size){
+	PIPE_CB* pipe = (PIPE_CB*) pipecb_t;
 
 	/*
 		checks if the pipe is closed or the writer or reader is closed
@@ -121,8 +171,8 @@ int pipe_write(void* write_index,const char* buf, unsigned int size){
 	 }
 
 	 /*
-	 	if the buffer has empty space let k be this space 
-		else if the buffer is empty let k equal the maximum buffer size 
+	 	if the buffer has empty space let j be this space 
+		else if the buffer is empty let j equal the maximum buffer size 
 	 */
 	 unsigned int j;
 	 if(size>free_space){
@@ -131,7 +181,7 @@ int pipe_write(void* write_index,const char* buf, unsigned int size){
 		j = size;
 	 }
 
-	 //Write opperation
+	 //Write operation
 
 	 for(int i=0; i<j; i++){
 		pipe->buffer[pipe->w_position+1]= buf[i];
@@ -140,7 +190,7 @@ int pipe_write(void* write_index,const char* buf, unsigned int size){
 	 }
 
 
-	kernel_broadcast(&(pipe->is_full));
+	kernel_broadcast(&(pipe->is_empty));
 
 
 
